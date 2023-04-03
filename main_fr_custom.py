081ec888d05460348a9012d04d01edc8d8e91d05
MarinaASUS <marinabar1609@gmail.com>
2023-04-03 16:04:34 +0200

    added comments & docstrings

--- main_fr_custom.py
+++ main_fr_custom.py
@@ -4,29 +4,37 @@ import pyttsx3
 
 
 engine = pyttsx3.init()
+
+# propriétés de la voix artificielle
 engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_FR-FR_HORTENSE_11.0')
 engine.setProperty('rate', 160)
 
-<<<<<<< HEAD
-
-openai.api_key="APIKEY"
-=======
 with open("apikey.txt", 'r') as f:
     openai.api_key=f.read()
->>>>>>> 22fe9f0 (chatbot complet, reconnait commande de début + fin)
 
 
 def audiowhisper(file):
+    """
+    Analyse un fichier audio vocal et renvoie le texte trouvé
+    """
     audio_file= open(file, "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    transcript = openai.Audio.transcribe("whisper-1", audio_file, 
+        prompt="Bonjour, je ne sais pas pourquoi j'ai, euh, une KeyError en Python",
+        language="fr")
     return transcript["text"]
 
 def text_to_audio(text):
+    """
+    Prononce le texte en entrée
+    """
     engine.say(text)
     engine.runAndWait()
 
 
 def generate_response(messages):
+    """
+    Génère la réponse de GPT3
+    """
     response= openai.ChatCompletion.create(
         model='gpt-3.5-turbo',
         messages=messages,
@@ -41,8 +49,8 @@ filename="input.wav"
 
 #liste des messages de notre conversation avec chatPT
 messages = [
-    {"role":"system", 'content':"Tu réponds avec une petite touche de passif aggressif, mais pas trop non plus"}
-]
+    {"role":"system", 'content':"Tu réponds avec une petite touche de passif aggressif"},
+    ]
 
 while run:
     # on écoute jusqu'à reconnaître le mot clé
@@ -60,6 +68,8 @@ while run:
 
                     while True:
                         print("j'attends votre réponse")
+
+                        # on enregistre la réponse
                         with sr.Microphone() as source:
                             r=sr.Recognizer()
                             r.adjust_for_ambient_noise(source)
@@ -67,6 +77,8 @@ while run:
                             with open(filename,"wb")as f:
                                 f.write(audiorep.get_wav_data())
                         print("analyse du texte")
+
+                        # analyse avec whisper
                         text=audiowhisper(filename)
 
                         #On vérifie si demande de sortie
